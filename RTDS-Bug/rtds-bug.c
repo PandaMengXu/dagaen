15dd5c7b586be2c0862ef18e08d627f4fde81c0f
Meng Xu <mengxu@cis.upenn.edu>
2016-10-19 11:46:51 -0400

    Add rdtsc to measure latency
    Jiffie is not precise enough to measure millesecond-level latency.
    As you can see, the result measured by Jiffie may vary within several ms, while the result measured by tsc is very stable.
    Jiffie is in 4ms precision.
    
    Signed-off-by: Meng Xu <mengxu@cis.upenn.edu>

--- RTDS-Bug/rtds-bug.c
+++ RTDS-Bug/rtds-bug.c
@@ -6,8 +6,27 @@
 #include <asm/xen/hypercall.h>
 #include <xen/xen.h>
 
+#define MHZ 2100
+
+#if defined(__i386__)
+static __inline__ unsigned long long rdtsc(void)
+{
+  unsigned long long int x;
+     __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+     return x;
+}
+#elif defined(__x86_64__)
+static __inline__ unsigned long long rdtsc(void)
+{
+  unsigned hi, lo;
+  __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+  return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
+}
+#endif
+
 static int __init lock_init (void) {
-	unsigned long start, end, max = 0;
+	unsigned long long start, end, max = 0;
+    unsigned long long start_tsc, end_tsc, max_tsc = 0, lat_tsc;
 	int counter;
 
 	printk(KERN_INFO "Testing for Hypercall Bug...\n");
@@ -17,15 +36,25 @@ static int __init lock_init (void) {
 		return -ENODEV;
 	}
 
-	for(counter = 0; counter < 1000000; ++counter) {
+	for(counter = 0; counter < 50000000; ++counter) {
+	//for(counter = 0; counter < 1000; ++counter) {
 		start = jiffies;
+        start_tsc = rdtsc();
 		HYPERVISOR_xen_version(0, NULL);
+        end_tsc = rdtsc();
 		end = jiffies_to_usecs(jiffies - start);
 
+        lat_tsc = ( end_tsc - start_tsc ) / MHZ;
 		if(end > max) max = end;
+        if ( lat_tsc > max_tsc )
+        {
+            printk("prev max: %llu us, new max: %llu us\n", max_tsc, lat_tsc);
+            max_tsc = lat_tsc;
+        }
 	}
 
-	printk(KERN_INFO "Max hyercall length: %lu us\n", max);
+	printk(KERN_INFO "Max hyercall length: %llu us\n", max);
+	printk(KERN_INFO "Max hyercall length (use tsc counter): %llu us\n", max_tsc);
 
 	return 0;
 }
